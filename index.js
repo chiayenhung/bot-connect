b637109727d6d2aa927f869d59a01b44801c4f2c
chiayenhung <hodalalala@gmail.com>
2017-09-30 00:46:13 -0700

    add restift

---
+++ index.js
@@ -0,0 +1,28 @@
+const restify = require("restify");
+const restifyBodyParser = require("restify-plugins").bodyParser;
+const request = require("request");
+
+const server = restify.createServer();
+server.use(restifyBodyParser());
+
+const DEFAULT_PROXY = "http://localhost:3000/api/messages";
+
+const defaultOptions = {
+  url: process.env.PROXY_URL || DEFAULT_PROXY
+};
+
+server.post("/api/messages", (req, res, next) => {
+  const body = req.body;
+  const options = Object.assign({}, defaultOptions, { body: JSON.stringify(body) });
+  request.post(options).pipe(res);
+  next();
+});
+
+server.get("/", (req, res, next) => {
+  res.send(`${server.name} listening at ${server.url}`);
+  next();
+});
+
+server.listen(8000, () => {
+  console.log('%s listening at %s', server.name, server.url);
+});
